6b9eaf7a68d8c25d19958d3619ade9f3150671f7
Samuel Carmona <samuel.carmona.rodrigz@gmail.com>
2022-11-10 23:28:19 +0000

    :new: We created the worst wordle ever! 🎁

---
+++ main.py
@@ -0,0 +1,43 @@
+
+# Esto es un comentario 🧡
+"""
+    esto es un comentario multilinea
+   dfsdf 
+   sdfsdf
+   sdfsdfsfsdf
+   sdf
+"""
+
+message = "Hello world 🥨"
+print(message)
+
+numerito = 1
+
+PI = 3.1415165454578413
+
+print(numerito)
+
+numero_real = 12.12
+
+#Aca no hoisting
+print(numero_real)
+
+cadena_real = "12.12"
+
+segundo_real = 12.12
+
+if numero_real == cadena_real:
+    print('se cumplio 👼🏽')
+
+elif numero_real == segundo_real:
+
+    print("se cumplio el elif 😳")
+
+else:
+    print('no se cumplio 🧨')
+
+def sum(a, b):
+    return a + b
+
+
+print(sum(0,7))
